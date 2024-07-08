907c64f56f09dad154ad230ba716b69f5a6c1857
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-08 14:16:36 +0600

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import * as THREE from 'three';
 import { OrbitControls } from "three-full/sources/controls/OrbitControls";
+import Stats from 'three/addons/libs/stats.module.js';
 
 const canvas = document.getElementById("webgl") as HTMLCanvasElement;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const stats = new Stats();
+stats.showPanel(0);
+document.body.appendChild(stats.dom);
+
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(10, 0, 0);
+camera.position.set(40, 0, 0);
 
 // Add orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -22,23 +28,27 @@ const uniforms = {
 }
 
 // Create a cube
-const planeGeometry = new THREE.PlaneGeometry(10, 10, 30, 30);
-const planeMaterial = new THREE.ShaderMaterial({
+const sphereGeometry = new THREE.SphereGeometry(20, 50, 50);
+const sphereMaterial = new THREE.ShaderMaterial({
   vertexShader: document.getElementById("vertex-shader")!.textContent!,
   fragmentShader: document.getElementById("fragment-shader")!.textContent!,
   side: THREE.DoubleSide,
-  // wireframe: true,
+  wireframe: true,
   uniforms,
 })
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.rotation.y = Math.PI / 2;
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
-scene.add(plane);
+scene.add(sphere);
 
+sphere.rotation.x -= 0.2;
 function animate() {
+  stats.begin();
   uniforms.u_time.value += 0.05;
+  controls.update();
+  sphere.rotation.y += 0.01;
 
   renderer.render(scene, camera);
+  stats.end();
 }
 
 renderer.setAnimationLoop(animate);
