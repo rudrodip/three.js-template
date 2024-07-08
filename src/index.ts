415105f2dc4b1f96de15a6927adb861e6517d63c
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-08 14:04:11 +0600

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as THREE from 'three-full';
+import * as THREE from 'three';
 import { OrbitControls } from "three-full/sources/controls/OrbitControls";
 
 const canvas = document.getElementById("webgl") as HTMLCanvasElement;
@@ -9,32 +9,47 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(3, 2, 3);
+camera.position.set(10, 0, 0);
 
 // Add orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
-// Axes helper
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
-// Grid helper
-const gridHelper = new THREE.GridHelper(30, 30);
-scene.add(gridHelper);
+const uniforms = {
+  u_time: { type: 'f', value: 0.0 },
+  u_resolution: { type: 'v2', value: new THREE.Vector2(window.innerWidth, window.innerHeight).multiplyScalar(window.devicePixelRatio) },
+  u_mouse: { type: 'v2', value: new THREE.Vector2(0.0, 0.0) },
+}
 
 // Create a cube
-const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const box = new THREE.Mesh(boxGeometry, boxMaterial);
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 30, 30);
+const planeMaterial = new THREE.ShaderMaterial({
+  vertexShader: document.getElementById("vertex-shader")!.textContent!,
+  fragmentShader: document.getElementById("fragment-shader")!.textContent!,
+  side: THREE.DoubleSide,
+  // wireframe: true,
+  uniforms,
+})
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.y = Math.PI / 2;
 
-scene.add(box);
+scene.add(plane);
 
 function animate() {
-  box.rotation.x += 0.01;
-  box.rotation.y += 0.01;
+  uniforms.u_time.value += 0.05;
 
   renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  uniforms.u_resolution.value.set(window.innerWidth, window.innerHeight).multiplyScalar(window.devicePixelRatio);
+})
+
+window.addEventListener('mousemove', (e) => {
+  uniforms.u_mouse.value.set(e.screenX / window.innerWidth, 1 - e.screenY / window.innerHeight);
+});
